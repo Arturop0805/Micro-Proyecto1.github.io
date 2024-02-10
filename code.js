4a8b6b9d3071c0187eaf4c1c39a6394b8552b859
Arturop0805 <122051444+Arturop0805@users.noreply.github.com>
2024-02-10 19:47:38 -0400

    Add files via upload

---
+++ code.js
@@ -0,0 +1,14 @@
+let tipo_matriz = 0;
+
+function Jugar() {
+    
+
+    const player1 = document.getElementById("Player1").value;
+    const player2 = document.getElementById("Player2").value;
+    const player3 = document.getElementById("Player3").value;
+    const player4 = document.getElementById("Player4").value;
+}
+
+const def_matriz = (a) => tipo_matriz = a;
+
+
